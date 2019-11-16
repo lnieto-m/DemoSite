39193a0053df264488b9d5f58ebffc209f651371
Luis NIETO MUNOZ <lnieto-m@student.42.fr>
2019-11-16 08:45:58 +0100

    chepa

--- src/components/Pages/pokedex/PokemonMainVue.vue
+++ src/components/Pages/pokedex/PokemonMainVue.vue
@@ -3,7 +3,15 @@
         <img src="@/assets/grey_pokeball.png" class="center"/>
         <!-- <img src="https://bulbapedia.bulbagarden.net/wiki/File:001MS.png" class="center" /> -->
     </div>
-    <div v-else id="pokevue">{{pokemonInfo}}</div>
+    <div v-else id="pokevue">
+        <img :src="spriteUrl" />
+        <div>
+            {{pokemonInfo.id}} {{pokemonInfo.name}}
+            Height : {{pokemonInfo.height}} m
+            Weight : {{pokemonInfo.weight}} kg
+        </div>
+        <!-- {{pokemonInfo}} -->
+    </div>
 </template>
 
 <script>
@@ -16,16 +24,70 @@ export default {
     data() {
         return {
             loaded: false,
-            pokemonInfo: {},
+            spriteUrl: "",
+            pokemonInfo: {
+                name: "",
+                id: 0,
+                height: 0,
+                weight: 0,
+                description: "",
+                stats: {
+                    Hp : 0,
+                    Atk: 0,
+                    Def: 0,
+                    "Sp. Atk": 0,
+                    "Sp. Def": 0,
+                    Speed: 0
+                    },
+            },
             display: "Nothing to see here"
         }
     },
     watch: {
         url: function () {
             axios.get(this.url)
-                .then(res => this.pokemonInfo = res.data)
-                .catch(err => console.log(err))
-            this.loaded = true
+                .then(res => {
+                    var exception = [
+                        "tapu",
+                        "jangmo",
+                        "hakamo",
+                        "kommo",
+                        "mr",
+                        "mime",
+                        "nidoran",
+                        "ho",
+                        "type"
+                    ]
+
+                    var spriteExceptions = {
+                        "tapu-koko":"tapukoko",
+                        "tapu-lele":"tapulele",
+                        "tapu-bulu":"tapubulu",
+                        "tapu-fini":"tapufini",
+                        "mr-mime": "mr._mime",
+                        "mime-jr": "mime_jr",
+                        "nidoran-m": "nidoran_m",
+                        "nidoran-f": "nidoran_f",
+                        "type-null": "typenull"
+                    }
+                    var name = ""
+                    if (exception.includes(res.data.species.name.split('-')[0]) == true) {
+                        name = res.data.species.name.replace('-', ' ')
+                    } else {
+                        name = res.data.species.name.split('-')[0]
+                    }
+                    this.pokemonInfo.name = name.charAt(0).toUpperCase() + name.slice(1)
+                    this.pokemonInfo.id = res.data.id
+                    this.pokemonInfo.height = res.data.height / 10
+                    this.pokemonInfo.weight = res.data.weight / 10
+                    if (res.data.species.name in spriteExceptions) {
+                        this.spriteUrl = "https://projectpokemon.org/images/normal-sprite/" + spriteExceptions[res.data.species.name] + ".gif"
+                    } else {
+                        this.spriteUrl = "https://projectpokemon.org/images/normal-sprite/" + res.data.species.name + ".gif"
+                    }
+                    this.loaded = true
+                })
+                .catch(err => console.log(err))      
         }
     }
 }
