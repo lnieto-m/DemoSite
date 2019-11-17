901995eebba478117cc42dcdede9502cd27983fb
Luis NIETO MUNOZ <lnieto-m@e2r12p11.42.fr>
2019-11-17 14:30:29 +0100

    added form redirections

--- src/components/Pages/pokedex/PokemonMainVue.vue
+++ src/components/Pages/pokedex/PokemonMainVue.vue
@@ -6,12 +6,13 @@
     <div v-else id="pokevue">
         <div id="mainview">
             <b-img style="max-height:100%;float:right" :src="spriteUrl" fluid></b-img>
-            <div style="float:left;margin-top:50px;">
-                {{pokemonInfo.id}} {{pokemonInfo.name}}<br/>
-                Height : {{pokemonInfo.height}} m<br/>
-                Weight : {{pokemonInfo.weight}} kg<br/>
+            <div style="float:left;margin-top:30px;">
+                {{pokemonInfo.name}}<br/>
+                {{pokemonInfo.genera}}<br/>
                 <b-badge v-bind:style="{backgroundColor: typeTable[pokemonInfo.type[0]]}">{{pokemonInfo.type[0]}}</b-badge>
                 <b-badge v-if="pokemonInfo.type.length > 1" v-bind:style="{backgroundColor: typeTable[pokemonInfo.type[1]]}">{{pokemonInfo.type[1]}}</b-badge><br />
+                Height : {{pokemonInfo.height}} m<br/>
+                Weight : {{pokemonInfo.weight}} kg<br/>
             </div>
         </div>
         <div>
@@ -33,8 +34,10 @@
                     <b-tab title="Evolution" active>
 
                     </b-tab>
-                    <b-tab title="Forms">
-
+                    <b-tab title="Forms" v-if="pokemonInfo.forms.length > 0">
+                        <b-list-group>
+                            <b-list-group-item button v-bind:key="form.pokemon.name" v-for="form in pokemonInfo.forms" v-on:click="updateUrl('https://pokeapi.co/api/v2/pokemon/'+form.pokemon.name)"><b-img :src="'https://projectpokemon.org/images/normal-sprite/'+form.pokemon.name+'.gif'"></b-img>{{form.pokemon.name}}</b-list-group-item>
+                        </b-list-group>
                     </b-tab>
                 </b-tabs>
             </div>
@@ -53,11 +56,13 @@ export default {
     props: ["url"],
     data() {
         return {
+            pokemonUrl : this.url,
             loaded: false,
             spriteUrl: "",
             pokemonInfo: {
                 name: "",
                 id: 0,
+                genera: "",
                 height: 0,
                 weight: 0,
                 description: "",
@@ -70,11 +75,12 @@ export default {
                     "Sp. Def":{value: 0, color: "0"},
                     Speed:{value: 0, color: "0"}
                 },
+                forms: [],
             },
             display: "Nothing to see here",
             typeTable: {
                 Bug: "#a8b820",
-                Dark: "#ffffff",
+                Dark: "#000000",
                 Dragon: "#4c08ef",
                 Electric: "#f0c108",
                 Fairy: "#F98CFF",
@@ -94,9 +100,18 @@ export default {
             }
         }
     },
+    methods: {
+        updateUrl: function (url) {
+            this.pokemonUrl = url
+            this.pokemonInfo.forms = []
+        }
+    },
     watch: {
-        url: function () {
-            axios.get(this.url)
+        url: function() {
+            this.pokemonUrl = this.url
+        },
+        pokemonUrl: function () {
+            axios.get(this.pokemonUrl)
                 .then(res => {
                     // Formating exceptions
                     var exception = [
@@ -123,9 +138,24 @@ export default {
                         "nidoran-f": "nidoran_f",
                         "type-null": "typenull"
                     }
+                    var formList = {
+                        "mega":"",
+                        "alola":"",
+                        "therian":"",
+                        "resolute":"",
+                        "pirouette":"",
+                        "sky":"",
+                        "white":"",
+                        "black":"",
+                        "ultra":""
+                    }
                     var name = ""
+                    var formCheck = res.data.name.split('-')
+
                     if (exception.includes(res.data.species.name.split('-')[0]) == true) {
                         name = res.data.species.name.replace('-', ' ')
+                    } else if (formCheck[formCheck.length - 1] in formList) {
+                        name = formCheck[0] + ' ' + formCheck[formCheck.length - 1].charAt(0).toUpperCase() + formCheck[formCheck.length - 1].slice(1)
                     } else {
                         name = res.data.species.name.split('-')[0]
                     }
@@ -137,8 +167,11 @@ export default {
                     this.pokemonInfo.weight = res.data.weight / 10
                     
                     // Get 3D sprite
+                    
                     if (res.data.species.name in spriteExceptions) {
                         this.spriteUrl = "https://projectpokemon.org/images/normal-sprite/" + spriteExceptions[res.data.species.name] + ".gif"
+                    } else if (formCheck[formCheck.length - 1] in formList) {
+                        this.spriteUrl = "https://projectpokemon.org/images/normal-sprite/" + res.data.name + ".gif"
                     } else {
                         this.spriteUrl = "https://projectpokemon.org/images/normal-sprite/" + res.data.species.name + ".gif"
                     }
@@ -159,15 +192,16 @@ export default {
 
                     // Get types
                     this.pokemonInfo.type = []
-                    this.pokemonInfo.type.push(res.data.types[0].type.name)
+                    this.pokemonInfo.type.push(res.data.types[0].type.name.charAt(0).toUpperCase() + res.data.types[0].type.name.slice(1))
                     if (res.data.types.length > 1) {
-                        this.pokemonInfo.type.push(res.data.types[1].type.name)
+                        this.pokemonInfo.type.push(res.data.types[1].type.name.charAt(0).toUpperCase() + res.data.types[1].type.name.slice(1))
                     }
                     this.pokemonInfo.type.reverse()
 
                     // Get Pokedex entry. For some reason it is not included in the base request
                     var descriptionURL = 'https://pokeapi.co/api/v2/pokemon-species/'+res.data.id
-                    axios.get(descriptionURL)
+                    try {
+                        axios.get(descriptionURL)
                         .then(res => {
                             for (var i = 0; i < res.data.flavor_text_entries.length; i++) {
                                 if (res.data.flavor_text_entries[i].language.name == 'en') {
@@ -175,8 +209,26 @@ export default {
                                     break
                                 }
                             }
+                            for (i = 0; i < res.data.genera.length; i++) {
+                                if (res.data.genera[i].language.name == 'en') {
+                                    this.pokemonInfo.genera = res.data.genera[i].genus
+                                    break
+                                }
+                            }
+                            this.pokemonInfo.forms = res.data.varieties
+                            this.pokemonInfo.forms.shift()
+                            if (res.data.name === "charizard" || res.data.name === "mewtwo") {
+                                for (i = 0; i < this.pokemonInfo.forms.length; i++) {
+                                    console.log(res.data.name)
+                                    var pos = this.pokemonInfo.forms[i].pokemon.name.lastOfIndex('-')
+                                    this.pokemonInfo.forms[i].pokemon.name = this.pokemonInfo.forms[i].pokemon.name.substring(0, pos) + this.pokemonInfo.forms[i].pokemon.name.substring(pos+1)
+                                }
+                            }
                         })
                         .catch(err => console.log(err))
+                    } catch {
+                        console.log("no page found")
+                    }
                     this.loaded = true
                 })
                 .catch(err => console.log(err))
